9b1bb24e99b4b0de49b06ec888a15041dcdd8abd
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-09-19 15:58:31 +0200

    lesson 4.3,4.4,4.5,4.6

---
+++ ppf-4.3.py
@@ -0,0 +1,38 @@
+game = [[1, 1, 1],
+        [0, 0, 0],
+        [2, 2, 0]]
+
+
+def win(current_game):
+  for row in game:
+    print(row)
+    if row.count(row[0]) == len(row) and row[0] != 0:
+      print("Winner!")
+
+
+win(game)
+
+# horizontally
+# vertically
+# daigonally
+
+
+# def game_board(game_map, player=0, row=0, column=0, just_display=False):
+#     try:
+#       print("   0  1  2 ")
+#       if not just_display:
+#         game_map[row][column] = player
+#       for count, row in enumerate(game_map):
+#         print(count, row)
+#       return game_map
+
+#     except IndexError as e:
+#       print("Error: did you input row/columns 0 1 or 2", e)
+
+#     except Exception as e:
+#       print("Something went very wrong!", e)
+
+
+# game = game_board(game, just_display=True)
+# game = game_board(game, player=1, row=3, column=1)
+# game = game_board(game_board, player=1, row=3, column=1)
---
+++ ppf-4.5.py
@@ -0,0 +1,75 @@
+game = [[2, 1, 1],
+        [1, 2, 0],
+        [2, 1, 2]]
+
+diags = []
+for col, row in enumerate(reversed(range(len(game)))):
+  diags.append(game[row][col])
+
+
+diags = []
+for ix in range(len(game)):
+  diags.append(game[ix][ix])
+
+
+# cols = reversed(range(len(game)))
+# rows = range(len(game))
+
+# for col, row in zip(cols, rows):
+#   print(col, row)
+
+# if game[0][0] == game[1][1] == game[1][1]:
+#   print("winner")
+
+
+# diags = []
+
+# for ix in range(len(game)):
+#   diags.append(game[ix][ix])
+# print (diags)
+
+
+# if game[2][0] == game[1][1] == game[0][2]:
+#   print("winner")
+
+# for col in range(len(game)):
+#   check = []
+#   for row in game:
+#     check.append(row[col])
+
+#   if check.count(check[0]) == len(check) and check[0] !=0:
+#     print("Winner!")
+
+# def win(current_game):
+#   for row in game:
+#     print(row)
+#     if row.count(row[0]) == len(row) and row[0] !=0:
+#       print("Winner!")
+
+
+# win(game)
+
+# horizontally
+# vertically
+# daigonally
+
+
+# def game_board(game_map, player=0, row=0, column=0, just_display=False):
+#     try:
+#       print("   0  1  2 ")
+#       if not just_display:
+#         game_map[row][column] = player
+#       for count, row in enumerate(game_map):
+#         print(count, row)
+#       return game_map
+
+#     except IndexError as e:
+#       print("Error: did you input row/columns 0 1 or 2", e)
+
+#     except Exception as e:
+#       print("Something went very wrong!", e)
+
+
+# game = game_board(game, just_display=True)
+# game = game_board(game, player=1, row=3, column=1)
+# game = game_board(game_board, player=1, row=3, column=1)
